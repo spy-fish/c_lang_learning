f87c5c273299416393a38a73e592018325e2fcf8
vadim <vadimbelous64@gmail.com>
2022-02-22 20:57:12 +0400

    упражнение 1.16

--- symarrays.c
+++ symarrays.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
-#define MAXLINE 100
+#define MAXLINE 10
 
 int getthisline(char str[] , int);
 void copy(char to[], char from[]);
 
+/*
+main version 2
+
+Переписал main предыдущей программы чтобы она могла
+печатать самую длинную строку без каких-либо
+ограничений на ее размер
+*/
 int main()
 {
-    int len, max;
+    int len, max, c;
     char line[MAXLINE];
     char longest[MAXLINE];
     max = 0;
-    while ((len = getthisline(line, MAXLINE)) > 0)
+    
+    while ((len = getthisline(line, MAXLINE)) > 0) {
+        if (line[len-1] != '\n')
+            while ((c = getchar()) != EOF && c != '\n')
+                ++len;
+
         if (len > max) {
             max = len;
             copy(longest, line);
         }
+    }
+    if (max < MAXLINE)
+        --max;
+
     if (max > 0)
-        printf("%s", longest);
+        printf("%s", "longest line: ");
+        printf("%s\n", longest);
+    
     return 0;
 }
 
