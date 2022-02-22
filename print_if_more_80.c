eb2db5db31c2de9da2c784091bc79c225df487b0
vadim <vadimbelous64@gmail.com>
2022-02-22 23:59:35 +0400

    n исправил на \n

--- print_if_more_80.c
+++ print_if_more_80.c
@@ -22,11 +22,10 @@ int getthisline(char s[], int lim)
     int c, i;
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
         s[i] = c;
-    if (c == 'n') {
+    if (c == '\n') {
         s[i] = c;
         ++i;
     }
     s[i] = '\0';
     return i;
 }
-
