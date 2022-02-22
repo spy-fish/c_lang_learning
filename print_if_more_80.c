86d1219b048a908162d44797a46d743ec4495dac
vadim <vadimbelous64@gmail.com>
2022-02-22 21:37:32 +0400

    упражнение 1.17

---
+++ print_if_more_80.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define MAXLINE 1000
+#define MORENLINES 80
+
+int getthisline(char str[], int);
+
+int main()
+{
+    int len;
+    char line[MAXLINE];
+
+    while((len = getthisline(line, MAXLINE)) > 0)
+        if ((len-1) > MORENLINES)
+            printf("%s", line);
+}
+
+// get this line func
+int getthisline(char s[], int lim)
+{
+    int c, i;
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == 'n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
